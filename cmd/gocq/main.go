e8b9bc81ef4a8d0ee073b460ab179b6c3c79d160
rock8526652 <137907707+rock8526652@users.noreply.github.com>
2023-09-22 19:25:42 +0800

    Update main.go

--- cmd/gocq/main.go
+++ cmd/gocq/main.go
@@ -458,8 +458,8 @@ func newClient() *client.QQClient {
 }
 
 var remoteVersions = map[int]string{
-	1: "https://raw.githubusercontent.com/RomiChan/protocol-versions/master/android_phone.json",
-	6: "https://raw.githubusercontent.com/RomiChan/protocol-versions/master/android_pad.json",
+	1: "https://raw.githubusercontent.com/MrXiaoM/qsign/mirai/txlib/8.9.70/android_phone.json",
+	6: "https://raw.githubusercontent.com/MrXiaoM/qsign/mirai/txlib/8.9.70/android_pad.json",
 }
 
 func getRemoteLatestProtocolVersion(protocolType int) ([]byte, error) {
